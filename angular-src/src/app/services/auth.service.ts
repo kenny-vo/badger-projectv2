1b1e8a839462dbe295c50856dac8240dfb952fde
Kenneth Vo <kennyvo91@gmail.com>
2017-07-28 12:23:16 -0500

    registration working

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class AuthService {
+  authToken: any;
+  user: any;
 
-  constructor() { }
+  constructor(private http:Http) { }
 
+  registerUser(user){
+    let headers = new Headers();
+    headers.append('Content-Type','application/json');
+    return this.http.post('http://localhost:3000/users/register', user,{headers: headers})
+      .map(res => res.json());
+  }
 }
