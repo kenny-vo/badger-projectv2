567ca219633f190b7a6fe10cc09dd836c5c9a3f4
Kenneth Vo <kennyvo91@gmail.com>
2017-07-28 09:09:10 -0500

    updated authentication routes

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,7 @@ const express    = require('express');
 const router     = express.Router();
 const passport   = require('passport');
 const jwt        = require('jsonwebtoken');
-
 const User       = require('../models/user');
-
 
 // Register
 router.post('/register', (req, res, next) => {
@@ -29,7 +27,36 @@ router.post('/register', (req, res, next) => {
 
 // Authenticate
 router.post('/authenticate', (req, res, next) => {
-  res.send('AUTHENTICATE')
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if(err) throw err;
+    if(!user) {
+      return res.json({success: false, msg: 'User not found.'});
+    }
+    User.comparePassword(password, user.password, (err, isMatch)=>{
+      if(err) throw err;
+      if(isMatch) {
+        const token = jwt.sign(user, config.secret, {
+          expiresIn: 604800 // 1 week
+        });
+
+        res.json({
+          success: true,
+          token: 'JWT '+token,
+          user: {
+            id: user._id,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email
+          }
+        });
+      } else {
+        res.json({success: false, msg:'Wrong password.'});
+      }
+    });
+  });
 });
 
 // Profile
--- server.js
+++ server.js
@@ -35,6 +35,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Body Parser Middlewear
 app.use(bodyParser.json());
 
+// Passport Middlewear
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
 app.use('/users', users)
 
 // Index Route
